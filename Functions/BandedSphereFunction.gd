8d8891e19d35c8758cbe80817521990558146521
Davi Hollanda <dav-holl@hotmail.com>
2020-12-05 17:06:36 -0300

    mexendo na esfera estrelada

--- Functions/BandedSphereFunction.gd
+++ Functions/BandedSphereFunction.gd
@@ -1,9 +1,9 @@
 extends Spatial
 
 static func graph_function(x, z, t):
-	var radius = 0.9 + 0.1 * sin(PI * (6.0 * x * 4.0 * z + t));
-	var perturbation = radius * cos(0.5 * PI * z + t);
+	var radius = 0.9 + 0.1 * sin(PI * (6.0 * x * 4.0 * z));
+	var perturbation = radius * cos(0.5 * PI * z);
 	var newX = perturbation * sin(PI * x)
 	var newY = radius * sin(PI * 0.5 *z)
-	var newZ = perturbation * cos(PI * x)
+	var newZ = perturbation * cos(PI * x + t)
 	return Vector3(newX, newY, newZ);
